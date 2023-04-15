54a9ef54f8e64833939f536db04d1df90bfa58e0
Jasik <rogozhkin87@gmail.com>
2023-04-15 15:11:37 +0900

    added project

---
+++ Organote/Organote/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Organote
+//
+//  Created by Vladimir Rogozhkin on 2023/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
